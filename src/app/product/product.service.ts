161c9e9364c8e698fb2c86e6ba0dd462799f2c48
VijiSubra <viji.subramanyan@yahoo.com>
2025-03-17 14:53:01 -0400

    listing products

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, ObservedValueOf } from 'rxjs';
 import { Product } from '../models/product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl + '/products';
+  constructor(private http: HttpClient ) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
