c47b17a1482f0062a54ea44d18d2f6dcfa5451d4
VijiSubra <viji.subramanyan@yahoo.com>
2025-03-17 14:26:02 -0400

    Added Modul, Component, service, environments

---
+++ src/app/product/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+}
