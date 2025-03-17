90659e4df8412e5f6e1122fbf7bd4f7a7a7a43c2
VijiSubra <viji.subramanyan@yahoo.com>
2025-03-17 15:37:12 -0400

    Designing wiht angular material

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-
-
+import { MatCardModule }  from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     ProductListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FlexLayoutModule
   ]
 })
 export class ProductModule { }
